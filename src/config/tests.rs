dd86114216d05123a8d4e48dcbf24c784a2519c2
Ariel Viera <ariel.viera@gmail.com>
2020-09-06 17:46:41 -0400

    Add tests for argument checking code

--- src/config/tests.rs
+++ src/config/tests.rs
@@ -1,11 +1,47 @@
 #[test]
-pub fn parse_erroneous_arguments(){
+pub fn parse_erroneous_arguments_1() {
     use super::*;
-    let parameters:Vec<String> = vec![String::from("--todo-file"),String::from("a")];
-    let iter:&mut dyn Iterator<Item=String> = &mut parameters.iter().map(|s| String::from(s));
-    if let Result::Err(ErrorType::MissingArguments(_)) =  parse_arguments(iter) {
-        println!("This is ok");
-    } else {
-        panic!("This test failed");
+    let parameters: Vec<&str> = vec!["--todo-file", "a"];
+    let iter: &mut dyn Iterator<Item = String> = &mut parameters.iter().map(|s| String::from(*s));
+    match parse_arguments(iter) {
+        Result::Err(ErrorType::MissingArguments(missing_arguments_vector)) => assert!(matches!(
+            missing_arguments_vector
+                .iter()
+                .find(|arg_def| match arg_def {
+                    ArgumentDef {
+                        long_form: "--done-file",
+                        ..
+                    } => true,
+                    _ => false,
+                }),
+            Option::Some(_)
+        )),
+        _ => {
+            panic!("This test failed");
+        }
+    }
+}
+
+#[test]
+pub fn parse_erroneous_arguments_2() {
+    use super::*;
+    let parameters: Vec<&str> = vec!["--done-file", "a"];
+    let iter: &mut dyn Iterator<Item = String> = &mut parameters.iter().map(|s| String::from(*s));
+    match parse_arguments(iter) {
+        Result::Err(ErrorType::MissingArguments(missing_arguments_vector)) => assert!(matches!(
+            missing_arguments_vector
+                .iter()
+                .find(|arg_def| match arg_def {
+                    ArgumentDef {
+                        long_form: "--todo-file",
+                        ..
+                    } => true,
+                    _ => false,
+                }),
+            Option::Some(_)
+        )),
+        _ => {
+            panic!("This test failed");
+        }
     }
 }
