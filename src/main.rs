239c45a2e0012e6a6cd3a01c26e54a76c98bd5c3
Ariel Viera <ariel.viera@gmail.com>
2020-09-17 19:42:09 -0400

    Renamed Config structure

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::env;
 use std::process::exit;
 use config::ErrorType;
 use model::*;
+use directories::*;
 
 #[derive(Debug)]
 struct Error {
@@ -23,7 +24,25 @@ impl std::convert::From<&str> for Error {
     }
 }
 
+fn run_app(todo_filename: String, done_filename: String, command:config::Command) -> Result<(), Error>{
+    let todo_str = std::fs::read_to_string(todo_filename)?;
+    let done_str = std::fs::read_to_string(done_filename)?;
+    let mut model = Model {
+        todo_data : TodoData::parse(&todo_str).expect("error parsing todo file"),
+        done_data : TodoData::parse(&done_str).expect("error parsing done file"),
+    };
+    model.execute(command).map_err(|e| Error{message: e.to_owned() })
+}
+
+fn read_configuration() -> config::ArgsConfig {
+    config::ArgsConfig {
+        todo_filename: Option::None,
+        done_filename: Option::None,
+    }
+}
+
 fn main() -> Result<(), Error>{
+    let config = read_configuration();
     match config::parse_arguments(&mut env::args()) {
         Result::Err(ErrorType::MissingArguments(unset_arguments)) =>  {
             for unset_argument in unset_arguments {
@@ -38,15 +57,12 @@ fn main() -> Result<(), Error>{
         #[allow(unused_variables)]
         Result::Ok(arguments) => {
             // println!("Config is {:#?}", config);
-            let todo_filename = arguments.config.todo_filename.unwrap();
-            let done_filename = arguments.config.done_filename.unwrap();
-            let todo_str = std::fs::read_to_string(todo_filename)?;
-            let done_str = std::fs::read_to_string(done_filename)?;
-            let mut model = Model {
-                todo_data : TodoData::parse(&todo_str).expect("error parsing todo file"),
-                done_data : TodoData::parse(&done_str).expect("error parsing done file"),
-            };
-            model.execute(arguments.command).map_err(|e| Error{message: e.to_owned() })
+            let param_todo_filename = arguments.config.todo_filename;
+            let param_done_filename = arguments.config.done_filename;
+
+            let todo_filename = param_todo_filename.or(config.todo_filename).expect("todo filename is not present in config file and --todo-file parameter is missing");
+            let done_filename = param_done_filename.or(config.done_filename).expect("done filename is not present in config file and --done-file parameter is missing");
+            run_app(todo_filename, done_filename, arguments.command)
         }
     }
 }
