f6fc5ac71eb888a99ea9b77ad3718288661c474c
Ariel Viera <ariel.viera@gmail.com>
2020-09-19 13:32:53 -0400

    Simplify application initialization

--- src/main.rs
+++ src/main.rs
@@ -29,12 +29,31 @@ impl std::convert::From<&str> for Error {
     }
 }
 
-fn run_app(
-    config: Config,
-    command:Command,
-) -> Result<(), Error> {
-    let todo_str = std::fs::read_to_string(config.todo_filename.unwrap())?;
-    let done_str = std::fs::read_to_string(config.done_filename.unwrap())?;
+impl std::convert::From<config::params::ErrorType> for Error {
+    fn from(error: config::params::ErrorType) -> Self {
+        match error {
+            ErrorType::MissingArguments(unset_arguments) => {
+                for unset_argument in unset_arguments {
+                    eprintln!("error unset argument {}", unset_argument.long_form);
+                }
+                Error {
+                    message: "missing arguments".into(),
+                }
+            }
+            ErrorType::CannotIdentifyCommand(_) => Error {
+                message: "error cannot identify command".into(),
+            },
+        }
+    }
+}
+
+fn run_app(config: Config, command: Command) -> Result<(), Error> {
+    let todo_str = std::fs::read_to_string(config.todo_filename.expect(
+        "todo filename is not present in config file and --todo-file parameter is missing",
+    ))?;
+    let done_str = std::fs::read_to_string(config.done_filename.expect(
+        "done filename is not present in config file and --done-file parameter is missing",
+    ))?;
     let mut model = Model {
         todo_data: TodoData::parse(&todo_str).expect("error parsing todo file"),
         done_data: TodoData::parse(&done_str).expect("error parsing done file"),
@@ -44,7 +63,7 @@ fn run_app(
     })
 }
 
-fn read_configuration(mut config: &mut Config){
+fn read_configuration(mut config: &mut Config) {
     if let Option::Some(dirs) = directories::ProjectDirs::from("", "", "todocommander") {
         let mut path = dirs.config_dir().to_path_buf();
         path.push("todocommander.cfg");
@@ -56,41 +75,13 @@ fn read_configuration(mut config: &mut Config){
         }
     }
 }
-                                                                                                                        
 fn main() -> Result<(), Error> {
     const VERSION: &'static str = env!("CARGO_PKG_VERSION");
     println!("{}", VERSION);
-    let mut config = Config::new(); 
+    let mut config = Config::new();
     read_configuration(&mut config);
     println!("{:?}", &config);
-    match parse_arguments(&mut env::args()) {
-        Result::Err(ErrorType::MissingArguments(unset_arguments)) => {
-            for unset_argument in unset_arguments {
-                eprintln!("error unset argument {}", unset_argument.long_form);
-            }
-            Result::Err(Error {
-                message: "missing arguments".into(),
-            })
-        }
-        Result::Err(ErrorType::CannotIdentifyCommand(_)) => {
-            eprintln!("error cannot identify command");
-            exit(2);
-        }
-        #[allow(unused_variables)]
-        Result::Ok(arguments) => {
-            // println!("Config is {:#?}", config);
-            let param_todo_filename = arguments.config.todo_filename;
-            let param_done_filename = arguments.config.done_filename;
-
-            config.todo_filename = param_todo_filename.or(config.todo_filename);
-            if config.todo_filename.is_none(){
-               panic!("todo filename is not present in config file and --todo-file parameter is missing");
-            };
-            config.done_filename = param_done_filename.or(config.done_filename);
-            if config.done_filename.is_none() {
-                panic!("done filename is not present in config file and --done-file parameter is missing");
-            };
-            run_app(config , arguments.command)
-        }
-    }
+    let arguments = parse_arguments(&mut env::args())?;
+    config.todo_filename = arguments.config.todo_filename.or(config.todo_filename);
+    run_app(config, arguments.command)
 }
